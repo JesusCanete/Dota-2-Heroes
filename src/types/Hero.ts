abb4a482aac66c6fa9c14c157933f016ffd7c13c
JesusCanete <jesus.canete971202@gmail.com>
2025-07-01 20:56:09 -0400

    fixing errors

--- src/types/Hero.ts
+++ src/types/Hero.ts
@@ -1,6 +1,6 @@
 // src/types/Hero.ts
 
-export type HeroAttribute = 'Fuerza' | 'Agilidad' | 'Inteligencia' | 'Universal';
+export type HeroAttribute = 'Strength' | 'Agility' | 'Intellect';
 export type HeroAttackType = 'Melee' | 'Ranged';
 export type HeroRole = 'Carry' | 'Support' | 'Nuker' | 'Disabler' | 'Jungler' | 'Durable' | 'Escape' | 'Pusher' | 'Initiator';
 
